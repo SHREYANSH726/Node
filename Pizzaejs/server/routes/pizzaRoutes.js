b28d944fd3a68fcfe0ae854034adee1d1421911f
SHREYANSH726 <lshreyansh726@gmail.com>
2025-08-01 10:29:10 +0530

    Delete Pizzaejs directory

--- Pizzaejs/server/routes/pizzaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const pizzaController = require("../pizzacontroller/pizzacontroller");
-
-router.get("/", pizzaController.getAllPizzas);
-router.post("/", pizzaController.createPizza);
-
-module.exports = router;
